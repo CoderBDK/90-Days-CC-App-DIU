b6f0492e1efa6bfb4af1a04d7aa22e7ef63d689d
CoderBDK <aicoder012@gmail.com>
2023-09-11 05:02:36 +0100

    Initial

---
+++ Kotlin programming language/src/main/java/com/coding/challenge/kotlinprogramminglanguage/_001HelloWorld.kt
@@ -0,0 +1,15 @@
+package com.coding.challenge.kotlinprogramminglanguage
+
+/**
+ * Here is the simple program that prints "Hello, World"
+ * @fun is used for declare a function
+ * the main() function is where program starts from
+ * {@link main()} The main function is executed when the program is run
+ * The main function is starting point for execution of program.
+ * the body of a function is written within curly braces :{}
+ */
+fun main(){
+    //println function print their arguments to standard output
+    println("Hello, World!")
+    //Hello, World!
+}
---
+++ Kotlin programming language/src/main/java/com/coding/challenge/kotlinprogramminglanguage/_002Variables.kt
@@ -0,0 +1,30 @@
+package com.coding.challenge.kotlinprogramminglanguage
+
+/**
+ * All programs need to be able store data and variables
+ * read-only variables with : val
+ * mutable variables with : var
+ * To assign a value in variable using assignment operator : =
+ */
+fun main(){
+    //mutable variable
+    var time = 0 // when start the time is 0s
+    println("---Mutable variable---")
+    println("1-Current time is : $time")
+
+    time = 10 //1: change the time reassign the value 10s
+    println("2-Current time is : $time")
+
+    time = 20 //2: at the end value 20s
+    println("3-Current time is : $time")
+
+    //read-only variable immutable
+    //init the val
+    val face = 1 // Humans have only one face
+    //face = 7 //compilation error : "Val cannot be reassigned"
+
+    println("---Read only variable---")
+    println("Humans have only one face: $face")
+
+
+}
