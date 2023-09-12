f3f42a6f75c728c0c9328df4d4611f5cefe24b33
CoderBDK <aicoder012@gmail.com>
2023-09-12 05:06:41 +0100

    Initial

---
+++ Kotlin programming language/src/main/java/com/coding/challenge/kotlinprogramminglanguage/basics/BasicSyntax.kt
@@ -0,0 +1,15 @@
+package com.coding.challenge.kotlinprogramminglanguage.basics
+
+// An entry point of a Kotlin application is the main function.
+fun main(){
+    println("Hello World!")
+    print("Hello ")
+    print("World!")
+}
+
+//  form of main accepts a variable number of String arguments.
+/*
+fun main(args: Array<String>) {
+    println(args.contentToString())
+}*/
+
---
+++ Kotlin programming language/src/main/java/com/coding/challenge/kotlinprogramminglanguage/basics/Functions.kt
@@ -0,0 +1,25 @@
+package com.coding.challenge.kotlinprogramminglanguage.basics
+
+fun main(){
+    println("Two parameter " + sum(5,7))
+    println("A Function body can be an Expression : ${sumInferred(5,7)}")
+    sumUnit(5,7)
+    sumOmitted(5,7)
+}
+
+// A function with two Int parameters and Int return type.
+fun sum(a: Int, b: Int): Int{
+    return a + b
+}
+// A function body can be an expression. Its return type is inferred.
+fun sumInferred(a: Int, b: Int) = a + b
+
+// A function that returns no meaningful value.
+fun sumUnit(a: Int, b: Int): Unit{
+    println("Unit function return no meaningful value: sum of $a and $b is ${a + b}")
+}
+
+// Unit return type can be omitted.
+fun sumOmitted(a: Int, b: Int){
+    println("Unit return type can be omitted : sum of $a and $b is ${a + b}")
+}
