4e13b6f318dd10fe2688d50b2fa1cb5d08c9e0cc
CoderBDK <cobdgithub07@gmail.com>
2023-11-10 08:51:11 +0000

    adaptive layouts

---
+++ appbasic/src/main/java/com/coderbdk/appbasic/ui/layout/AdaptiveLayouts.kt
@@ -0,0 +1,55 @@
+package com.coderbdk.appbasic.ui.layout
+
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Preview
+@Composable
+fun AdaptiveLayouts(){
+    AdaptivePane(showOnePane = true)
+}
+@Composable
+fun AdaptivePane(
+    showOnePane: Boolean,
+) {
+    if (showOnePane) {
+        Column(
+            Modifier.fillMaxSize()
+        ) {
+            OnePane()
+            Card()
+        }
+
+    } else {
+        TwoPane()
+    }
+}
+
+@Composable
+fun TwoPane() {
+
+}
+
+@Composable
+fun OnePane() {
+    Text(text = "hello, world")
+}
+@Composable
+fun Card() {
+    BoxWithConstraints(
+        Modifier.fillMaxSize()
+    ) {
+        if (maxWidth < 400.dp) {
+            Text(text = "400.dp > :: hello, world")
+        } else {
+            Text(text = "hello, world")
+        }
+    }
+}
