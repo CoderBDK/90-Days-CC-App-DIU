22d60129bee2f498672b75c60007dffe9f6bd130
CoderBDK <cobdgithub07@gmail.com>
2023-11-22 01:35:33 +0000

    slider

---
+++ appbasic/src/main/java/com/coderbdk/appbasic/ui/components/Slider.kt
@@ -0,0 +1,80 @@
+package com.coderbdk.appbasic.ui.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RangeSlider
+import androidx.compose.material3.Slider
+import androidx.compose.material3.SliderDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun SliderPreview(){
+    Column {
+        SliderMinimalExample()
+        SliderAdvancedExample()
+        RangeSliderExample()
+    }
+
+}
+
+@Preview
+@Composable
+fun SliderMinimalExample() {
+    var sliderPosition by remember { mutableFloatStateOf(0f) }
+    Column {
+        Slider(
+            value = sliderPosition,
+            onValueChange = { sliderPosition = it }
+        )
+        Text(text = sliderPosition.toString())
+    }
+}
+
+@Preview
+@Composable
+fun SliderAdvancedExample() {
+    var sliderPosition by remember { mutableFloatStateOf(0f) }
+    Column {
+        Slider(
+            value = sliderPosition,
+            onValueChange = { sliderPosition = it },
+            colors = SliderDefaults.colors(
+                thumbColor = MaterialTheme.colorScheme.secondary,
+                activeTrackColor = MaterialTheme.colorScheme.secondary,
+                inactiveTrackColor = MaterialTheme.colorScheme.secondaryContainer,
+            ),
+            steps = 3,
+            valueRange = 0f..50f
+        )
+        Text(text = sliderPosition.toString())
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
+@Composable
+fun RangeSliderExample() {
+    var sliderPosition by remember { mutableStateOf(0f..100f) }
+    Column {
+        RangeSlider(
+            value = sliderPosition,
+            steps = 10,
+            onValueChange = { range -> sliderPosition = range },
+            valueRange = 0f..100f,
+            onValueChangeFinished = {
+                // launch some business logic update with the state you hold
+                // viewModel.updateSelectedSliderValue(sliderPosition)
+            },
+        )
+        Text(text = sliderPosition.toString())
+    }
+}
