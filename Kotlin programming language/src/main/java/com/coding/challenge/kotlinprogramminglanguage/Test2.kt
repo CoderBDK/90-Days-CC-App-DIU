d2318a32af6185d01be35f18dd36d01d6b1dfb4e
CoderBDK <cobdgithub07@gmail.com>
2023-11-27 08:18:19 +0000

    Revert "lists and grids"
    This reverts commit 60632496bab7ddb2d7414cfa0c4595c1f58535a3.

--- Kotlin programming language/src/main/java/com/coding/challenge/kotlinprogramminglanguage/Test2.kt
+++ Kotlin programming language/src/main/java/com/coding/challenge/kotlinprogramminglanguage/Test2.kt
@@ -1,28 +1,10 @@
 package com.coding.challenge.kotlinprogramminglanguage
-
-import kotlin.math.sqrt
-
-fun sqrt(i: Int): Int {
-    return sqrt(i.toDouble()).toInt()
-}
+import java.net.URL
+import java.util.Scanner
+import kotlin.math.min
 
 fun main() {
-    val n = 100
-    val primes = Array(n) { 0 }
-    // println(primes.contentToString())
-    for (i in 2 until n) {
-        if (primes[i] == 0) {
-            for (j in 2 until sqrt(i)) {
-                if (i % j == 0) {
-                    primes[i] = 1
-                }
-            }
-        }
-    }
 
-    for (i in 2 until n) {
-        if (primes[i] == 0) {
-            println("$i -> prime")
-        }
-    }
+
+
 }
