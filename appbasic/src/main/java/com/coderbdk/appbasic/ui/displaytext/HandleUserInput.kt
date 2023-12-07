2e19d57bf6c599fa29015fcf0009c0caa0728b02
CoderBDK <cobdgithub07@gmail.com>
2023-12-07 01:48:38 +0000

    handle user input

---
+++ appbasic/src/main/java/com/coderbdk/appbasic/ui/displaytext/HandleUserInput.kt
@@ -0,0 +1,91 @@
+package com.coderbdk.appbasic.ui.displaytext
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Preview
+@Composable
+fun HandleUserInput(){
+    Column {
+        SimpleFilledTextFieldSample()
+        SimpleOutlinedTextFieldSample()
+        StyledTextField()
+        PasswordTextField()
+        NoLeadingZeroes()
+    }
+}
+@Composable
+fun SimpleFilledTextFieldSample() {
+    var text by remember { mutableStateOf("Hello") }
+
+    TextField(
+        value = text,
+        onValueChange = { text = it },
+        label = { Text("Label") }
+    )
+}
+@Composable
+fun SimpleOutlinedTextFieldSample() {
+    var text by remember { mutableStateOf("") }
+
+    OutlinedTextField(
+        value = text,
+        onValueChange = { text = it },
+        label = { Text("Label") }
+    )
+}
+@Composable
+fun StyledTextField() {
+    var value by remember { mutableStateOf("Hello\nWorld\nInvisible") }
+
+    TextField(
+        value = value,
+        onValueChange = { value = it },
+        label = { Text("Enter text") },
+        maxLines = 2,
+        textStyle = TextStyle(color = Color.Blue, fontWeight = FontWeight.Bold),
+        modifier = Modifier.padding(20.dp)
+    )
+}
+
+@Composable
+fun PasswordTextField() {
+    var password by rememberSaveable { mutableStateOf("") }
+
+    TextField(
+        value = password,
+        onValueChange = { password = it },
+        label = { Text("Enter password") },
+        visualTransformation = PasswordVisualTransformation(),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+    )
+}
+
+@Composable
+fun NoLeadingZeroes() {
+    var input by rememberSaveable { mutableStateOf("") }
+    TextField(
+        value = input,
+        onValueChange = { newText ->
+            input = newText.trimStart { it == '0' }
+        }
+    )
+}
