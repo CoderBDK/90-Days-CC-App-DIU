8876bf4db8d43910d68056db756d72f018d96eeb
CoderBDK <cobdgithub07@gmail.com>
2023-12-15 12:22:46 +0000

    custom painter

---
+++ appbasic/src/main/java/com/coderbdk/appbasic/ui/images/CustomPainter.kt
@@ -0,0 +1,87 @@
+package com.coderbdk.appbasic.ui.images
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.toSize
+import com.coderbdk.appbasic.R
+import kotlin.math.roundToInt
+
+@Preview
+@Composable
+fun CustomPainter(){
+    val rainbowImage = ImageBitmap.imageResource(id = R.drawable.ic_android_black_24dp)
+    val dogImage = ImageBitmap.imageResource(id = R.drawable.ic_android_black_24dp)
+    val customPainter = remember {
+        OverlayImagePainter(dogImage, rainbowImage)
+    }
+    Image(
+        painter = customPainter,
+        contentDescription = "android",
+        contentScale = ContentScale.Crop,
+        modifier = Modifier.wrapContentSize()
+    )
+}
+
+class OverlayImagePainter constructor(
+    private val image: ImageBitmap,
+    private val imageOverlay: ImageBitmap,
+    private val srcOffset: IntOffset = IntOffset.Zero,
+    private val srcSize: IntSize = IntSize(image.width, image.height),
+    private val overlaySize: IntSize = IntSize(imageOverlay.width, imageOverlay.height)
+) : Painter() {
+
+    private val size: IntSize = validateSize(srcOffset, srcSize)
+    override fun DrawScope.onDraw() {
+        // draw the first image without any blend mode
+        drawImage(
+            image,
+            srcOffset,
+            srcSize,
+            dstSize = IntSize(
+                this@onDraw.size.width.roundToInt(),
+                this@onDraw.size.height.roundToInt()
+            )
+        )
+        // draw the second image with an Overlay blend mode to blend the two together
+        drawImage(
+            imageOverlay,
+            srcOffset,
+            overlaySize,
+            dstSize = IntSize(
+                this@onDraw.size.width.roundToInt(),
+                this@onDraw.size.height.roundToInt()
+            ),
+            blendMode = BlendMode.Overlay
+        )
+    }
+
+    /**
+     * Return the dimension of the underlying [ImageBitmap] as it's intrinsic width and height
+     */
+    override val intrinsicSize: Size get() = size.toSize()
+
+    private fun validateSize(srcOffset: IntOffset, srcSize: IntSize): IntSize {
+        require(
+            srcOffset.x >= 0 &&
+                    srcOffset.y >= 0 &&
+                    srcSize.width >= 0 &&
+                    srcSize.height >= 0 &&
+                    srcSize.width <= image.width &&
+                    srcSize.height <= image.height
+        )
+        return srcSize
+    }
+}
