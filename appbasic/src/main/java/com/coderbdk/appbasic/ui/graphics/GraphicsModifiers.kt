46d420b856669c7b6b6da47c154f22b4addf44bc
CoderBDK <cobdgithub07@gmail.com>
2023-12-18 03:50:51 +0000

    Graphics modifiers

---
+++ appbasic/src/main/java/com/coderbdk/appbasic/ui/graphics/GraphicsModifiers.kt
@@ -0,0 +1,106 @@
+package com.coderbdk.appbasic.ui.graphics
+
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.draw.drawWithCache
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.colorListOf
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.coderbdk.appbasic.R
+
+@Preview
+@Composable
+fun GraphicsModifiers(){
+
+    var pointerOffset by remember {
+        mutableStateOf(Offset(0f, 0f))
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .pointerInput("dragging") {
+                detectDragGestures { change, dragAmount ->
+                    pointerOffset += dragAmount
+                }
+            }
+            .onSizeChanged {
+                pointerOffset = Offset(it.width / 2f, it.height / 2f)
+            }
+            .drawWithContent {
+                drawContent()
+                // draws a fully black area with a small keyhole at pointerOffset that’ll show part of the UI.
+                drawRect(
+                    Brush.radialGradient(
+                        listOf(Color.Transparent, Color.Black),
+                        center = pointerOffset,
+                        radius = 100.dp.toPx(),
+                    )
+                )
+            }
+    ) {
+
+        Text(
+            text = stringResource(id = R.string.paragraph),
+            fontSize = 100.sp,
+            style = TextStyle(
+                brush = Brush.radialGradient(
+                    colors = listOf(Color.Blue, Color.Red, Color.Magenta)
+                )
+            ),
+            modifier = Modifier
+                .shadow(100.dp)
+        )
+        Text(
+            "Hello Compose!",
+            modifier = Modifier
+                .drawBehind {
+                    drawRoundRect(
+                        Color(0xFFBBAAEE),
+                        cornerRadius = CornerRadius(10.dp.toPx())
+                    )
+                }
+                .padding(4.dp)
+        )
+        Text(
+            "Hello Compose!",
+            modifier = Modifier
+                .drawWithCache {
+                    val brush = Brush.linearGradient(
+                        listOf(
+                            Color(0xFF9E82F0),
+                            Color(0xFF42A5F5)
+                        )
+                    )
+                    onDrawBehind {
+                        drawRoundRect(
+                            brush,
+                            cornerRadius = CornerRadius(10.dp.toPx())
+                        )
+                    }
+                }
+        )
+    }
+
+}
