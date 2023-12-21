1a0d7e2a32aebe4d1a179e2484436fd53adc3c91
CoderBDK <cobdgithub07@gmail.com>
2023-12-21 02:05:25 +0000

    animation modifiers

---
+++ appbasic/src/main/java/com/coderbdk/appbasic/ui/animation/AnimationModifiers.kt
@@ -0,0 +1,84 @@
+package com.coderbdk.appbasic.ui.animation
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+/**
+ * Created by MD. ABDULLAH on Thu, Dec 21, 2023.
+ */
+
+@OptIn(ExperimentalAnimationApi::class)
+@Preview
+@Composable
+fun AnimationModifiers(){
+    var visible by remember { mutableStateOf(true) }
+    val density = LocalDensity.current
+    Column {
+
+        AnimatedVisibility(
+            visible = visible,
+            enter = slideInVertically {
+                // Slide in from 40 dp from the top.
+                with(density) { -40.dp.roundToPx() }
+            } + expandVertically(
+                // Expand from the top.
+                expandFrom = Alignment.Top
+            ) + fadeIn(
+                // Fade in with the initial alpha of 0.3f.
+                initialAlpha = 0.3f
+            ),
+            exit = slideOutVertically() + shrinkVertically() + fadeOut()
+        ) {
+            Text("Hello", Modifier.fillMaxWidth().height(50.dp))
+        }
+
+
+        AnimatedVisibility(
+            visible = visible,
+            enter = fadeIn(),
+            exit = fadeOut()
+        ) {
+            // Fade in/out the background and the foreground.
+            Box(Modifier.fillMaxWidth().height(50.dp).background(Color.DarkGray)) {
+                Box(
+                    Modifier
+                        .align(Alignment.Center)
+                        .animateEnterExit(
+                            // Slide in/out the inner box.
+                            enter = slideInVertically(),
+                            exit = slideOutVertically()
+                        )
+                        .sizeIn(minWidth = 256.dp, minHeight = 64.dp)
+                        .background(Color.Red)
+                ) {
+                    // Content of the notification…
+                }
+            }
+        }
+    }
+}
