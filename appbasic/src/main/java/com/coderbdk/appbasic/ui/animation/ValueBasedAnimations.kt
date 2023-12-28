e2b480c89e77e672025a304086d040a1091ec90e
CoderBDK <cobdgithub07@gmail.com>
2023-12-28 07:36:15 +0000

    valued based animations

--- appbasic/src/main/java/com/coderbdk/appbasic/ui/animation/ValueBasedAnimations.kt
+++ appbasic/src/main/java/com/coderbdk/appbasic/ui/animation/ValueBasedAnimations.kt
@@ -1,12 +1,20 @@
 package com.coderbdk.appbasic.ui.animation
 
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.animateDp
 import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.animateRect
+import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.tween
+import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -14,6 +22,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.tooling.preview.Preview
@@ -45,5 +55,62 @@ fun ValueBasedAnimation() {
         Button(onClick = { enable = !enable }) {
             
         }
+
+        var currentState by remember { mutableStateOf(BoxState.Collapsed) }
+        val transition = updateTransition(currentState, label = "box state")
+        val rect by transition.animateRect(label = "rectangle") { state ->
+            when (state) {
+                BoxState.Collapsed -> Rect(0f, 0f, 100f, 100f)
+                BoxState.Expanded -> Rect(100f, 100f, 300f, 300f)
+            }
+        }
+        val borderWidth by transition.animateDp(label = "border width") { state ->
+            when (state) {
+                BoxState.Collapsed -> 1.dp
+                BoxState.Expanded -> 0.dp
+            }
+        }
+
+        val color by transition.animateColor(
+            transitionSpec = {
+                when {
+                    BoxState.Expanded isTransitioningTo BoxState.Collapsed ->
+                        spring(stiffness = 50f)
+                    else ->
+                        tween(durationMillis = 500)
+                }
+            }, label = "color"
+        ) { state ->
+            when (state) {
+                BoxState.Collapsed -> MaterialTheme.colorScheme.primary
+                BoxState.Expanded -> MaterialTheme.colorScheme.background
+            }
+        }
+
+        Box(
+            Modifier
+                .size(100.dp)
+                .background(color)
+                .border(width = borderWidth,
+                    brush = Brush.linearGradient(
+                        listOf(
+                            Color.Red,
+                            Color.Blue
+                        )
+                    ), shape = RoundedCornerShape(10.dp)
+                )
+        ) {
+
+        }
+        Button(onClick = {
+            currentState = BoxState.Expanded
+        }) {
+
+        }
     }
+}
+
+enum class BoxState {
+    Collapsed,
+    Expanded
 }
