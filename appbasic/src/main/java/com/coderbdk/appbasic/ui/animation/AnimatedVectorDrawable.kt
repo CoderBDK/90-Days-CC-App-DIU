a1f67f42de7c4de7835b544da6cf682e5d877849
CoderBDK <cobdgithub07@gmail.com>
2023-12-31 07:53:34 +0000

    animated vector drawable

---
+++ appbasic/src/main/java/com/coderbdk/appbasic/ui/animation/AnimatedVectorDrawable.kt
@@ -0,0 +1,37 @@
+package com.coderbdk.appbasic.ui.animation
+
+import androidx.compose.animation.graphics.ExperimentalAnimationGraphicsApi
+import androidx.compose.animation.graphics.res.animatedVectorResource
+import androidx.compose.animation.graphics.res.rememberAnimatedVectorPainter
+import androidx.compose.animation.graphics.vector.AnimatedImageVector
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.tooling.preview.Preview
+import com.coderbdk.appbasic.R
+
+/**
+ * Created by MD. ABDULLAH on Sun, Dec 31, 2023.
+ */
+
+@OptIn(ExperimentalAnimationGraphicsApi::class)
+@Preview
+@Composable
+fun AnimatedVectorDrawable() {
+    val image = AnimatedImageVector.animatedVectorResource(R.drawable.vector_drawable)
+    var atEnd by remember { mutableStateOf(false) }
+    Image(
+        painter = rememberAnimatedVectorPainter(image, atEnd),
+        contentDescription = "Timer",
+        modifier = Modifier.clickable {
+            atEnd = !atEnd
+        },
+        contentScale = ContentScale.Crop
+    )
+}
