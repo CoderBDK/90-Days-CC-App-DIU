5040b125be0663c935fdb56a4f829613def7fb13
CoderBDK <cobdgithub07@gmail.com>
2024-01-01 04:41:30 +0000

    advanced animation

---
+++ appbasic/src/main/java/com/coderbdk/appbasic/ui/animation/AdvancedAnimation.kt
@@ -0,0 +1,139 @@
+package com.coderbdk.appbasic.ui.animation
+
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.VectorConverter
+import androidx.compose.animation.core.calculateTargetValue
+import androidx.compose.animation.splineBasedDecay
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.awaitFirstDown
+import androidx.compose.foundation.gestures.horizontalDrag
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.input.pointer.positionChange
+import androidx.compose.ui.input.pointer.util.VelocityTracker
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import kotlin.math.absoluteValue
+import kotlin.math.roundToInt
+
+/**
+ * Created by MD. ABDULLAH on Mon, Jan 01, 2024.
+ */
+
+@Preview
+@Composable
+fun AdvancedAnimation(){
+    Column(
+        Modifier.fillMaxSize()
+    ) {
+        Gesture()
+    }
+
+}
+
+@Composable
+fun Gesture() {
+    val offset = remember { Animatable(Offset(0f, 0f), Offset.VectorConverter) }
+    Box(
+        modifier = Modifier
+            .size(200.dp)
+            .background(Color.Green)
+            .pointerInput(Unit) {
+                coroutineScope {
+                    while (true) {
+                        // Detect a tap event and obtain its position.
+                        awaitPointerEventScope {
+                            val position = awaitFirstDown().position
+
+                            launch {
+                                // Animate to the tap position.
+                                offset.animateTo(position)
+                            }
+                        }
+                    }
+                }
+            }
+    ) {
+        Box(modifier = Modifier
+            .size(100.dp)
+            .swipeToDismiss {
+
+            }
+            .background(Color.Red)
+            .offset { offset.value.toIntOffset() })
+    }
+}
+private fun Offset.toIntOffset() = IntOffset(x.roundToInt(), y.roundToInt())
+
+fun Modifier.swipeToDismiss(
+    onDismissed: () -> Unit
+): Modifier = composed {
+    val offsetX = remember { Animatable(0f) }
+    pointerInput(Unit) {
+        // Used to calculate fling decay.
+        val decay = splineBasedDecay<Float>(this)
+        // Use suspend functions for touch events and the Animatable.
+        coroutineScope {
+            while (true) {
+                val velocityTracker = VelocityTracker()
+                // Stop any ongoing animation.
+                offsetX.stop()
+                awaitPointerEventScope {
+                    // Detect a touch down event.
+                    val pointerId = awaitFirstDown().id
+
+                    horizontalDrag(pointerId) { change ->
+                        // Update the animation value with touch events.
+                        launch {
+                            offsetX.snapTo(
+                                offsetX.value + change.positionChange().x
+                            )
+                        }
+                        velocityTracker.addPosition(
+                            change.uptimeMillis,
+                            change.position
+                        )
+                    }
+                }
+                // No longer receiving touch events. Prepare the animation.
+                val velocity = velocityTracker.calculateVelocity().x
+                val targetOffsetX = decay.calculateTargetValue(
+                    offsetX.value,
+                    velocity
+                )
+                // The animation stops when it reaches the bounds.
+                offsetX.updateBounds(
+                    lowerBound = -size.width.toFloat(),
+                    upperBound = size.width.toFloat()
+                )
+                launch {
+                    if (targetOffsetX.absoluteValue <= size.width) {
+                        // Not enough velocity; Slide back.
+                        offsetX.animateTo(
+                            targetValue = 0f,
+                            initialVelocity = velocity
+                        )
+                    } else {
+                        // The element was swiped away.
+                        offsetX.animateDecay(velocity, decay)
+                        onDismissed()
+                    }
+                }
+            }
+        }
+    }
+        .offset { IntOffset(offsetX.value.roundToInt(), 0) }
+}
