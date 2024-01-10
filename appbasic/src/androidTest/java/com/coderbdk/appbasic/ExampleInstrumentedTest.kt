0ef1faaea5bb4616bd54884f2ccef00d6d465c74
CoderBDK <cobdgithub07@gmail.com>
2024-01-10 10:31:36 +0000

    animation test init

--- appbasic/src/androidTest/java/com/coderbdk/appbasic/ExampleInstrumentedTest.kt
+++ appbasic/src/androidTest/java/com/coderbdk/appbasic/ExampleInstrumentedTest.kt
@@ -1,5 +1,20 @@
 package com.coderbdk.appbasic
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.test.captureToImage
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.unit.dp
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
@@ -7,6 +22,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -20,5 +36,37 @@ class ExampleInstrumentedTest {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.coderbdk.appbasic.test", appContext.packageName)
+
+    }
+
+    @get:Rule
+    val rule = createComposeRule()
+
+    @Test
+    fun testAnimationWithClock() {
+        // Pause animations
+        rule.mainClock.autoAdvance = false
+        var enabled by mutableStateOf(false)
+        rule.setContent {
+            val color by animateColorAsState(
+                targetValue = if (enabled) Color.Red else Color.Green,
+                animationSpec = tween(durationMillis = 250), label = ""
+            )
+            Box(Modifier.size(64.dp).background(color))
+        }
+
+        // Initiate the animation.
+        enabled = true
+
+        // Let the animation proceed.
+        rule.mainClock.advanceTimeBy(50L)
+
+        // Compare the result with the image showing the expected result.
+        // `assertAgainGolden` needs to be implemented in your code.
+        rule.onRoot().captureToImage()
+    }
+
+    private fun ImageBitmap.assertAgainstGolden(){
+
     }
 }
