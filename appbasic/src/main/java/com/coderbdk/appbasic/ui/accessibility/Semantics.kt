00665461790145e719f8d6a575e9617128add53a
CoderBDK <cobdgithub07@gmail.com>
2024-01-14 04:43:50 +0000

    semantics

---
+++ appbasic/src/main/java/com/coderbdk/appbasic/ui/accessibility/Semantics.kt
@@ -0,0 +1,14 @@
+package com.coderbdk.appbasic.ui.accessibility
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+/**
+ * Created by MD. ABDULLAH on Thu, Jan 11, 2024.
+ */
+
+@Preview
+@Composable
+fun Semantics() {
+
+}
