5de636a9e158a80db0b8ae3a0b7b27d4af9dee9e
CoderBDK <cobdgithub07@gmail.com>
2024-01-15 02:23:39 +0000

    scroll modifier and nested scrolling

---
+++ appbasic/src/main/java/com/coderbdk/appbasic/ui/touchinput/Scroll.kt
@@ -0,0 +1,92 @@
+package com.coderbdk.appbasic.ui.touchinput
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+/**
+ * Created by MD. ABDULLAH on Mon, Jan 15, 2024.
+ */
+
+@Preview
+@Composable
+fun Scroll() {
+    // ScrollBoxes();
+    // ScrollBoxesSmooth()
+    AutomaticNestedScroll()
+}
+@Composable
+private fun ScrollBoxes() {
+    Column(
+        modifier = Modifier
+            .background(Color.LightGray)
+            .size(100.dp)
+            .verticalScroll(rememberScrollState())
+    ) {
+        repeat(10) {
+            Text("Item $it", modifier = Modifier.padding(2.dp))
+        }
+    }
+}
+@Composable
+private fun ScrollBoxesSmooth() {
+    // Smoothly scroll 100px on first composition
+    val state = rememberScrollState()
+    LaunchedEffect(Unit) { state.animateScrollTo(100) }
+
+    Column(
+        modifier = Modifier
+            .background(Color.LightGray)
+            .size(100.dp)
+            .padding(horizontal = 8.dp)
+            .verticalScroll(state)
+    ) {
+        repeat(10) {
+            Text("Item $it", modifier = Modifier.padding(2.dp))
+        }
+    }
+}
+
+@Composable
+private fun AutomaticNestedScroll() {
+    val gradient = Brush.verticalGradient(0f to Color.Gray, 1000f to Color.White)
+    Box(
+        modifier = Modifier
+            .background(Color.LightGray)
+            .verticalScroll(rememberScrollState())
+            .padding(32.dp)
+    ) {
+        Column {
+            repeat(6) {
+                Box(
+                    modifier = Modifier
+                        .height(128.dp)
+                        .verticalScroll(rememberScrollState())
+                ) {
+                    Text(
+                        "Scroll here",
+                        modifier = Modifier
+                            .border(12.dp, Color.DarkGray)
+                            .background(brush = gradient)
+                            .padding(24.dp)
+                            .height(150.dp)
+                    )
+                }
+            }
+        }
+    }
+}
