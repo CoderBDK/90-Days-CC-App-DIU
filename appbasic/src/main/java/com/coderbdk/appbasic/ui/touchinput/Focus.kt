441bd401331b555bb0cd9c17021467ceb2d541bb
CoderBDK <cobdgithub07@gmail.com>
2024-01-20 15:11:25 +0000

    Input - Change Focus traversal

--- appbasic/src/main/java/com/coderbdk/appbasic/ui/touchinput/Focus.kt
+++ appbasic/src/main/java/com/coderbdk/appbasic/ui/touchinput/Focus.kt
@@ -6,16 +6,25 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusProperties
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.tooling.preview.Preview
 
 /**
  * Created by MD. ABDULLAH on Fri, Jan 19, 2024.
  */
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Preview
 @Composable
 fun Focus() {
+
+    val (first, second, third, fourth) = remember { FocusRequester.createRefs() }
+
     Column(
         Modifier.fillMaxSize()
     ) {
@@ -39,6 +48,28 @@ fun Focus() {
                 TextButton({ }) { Text("Fourth field") }
             }
         }
+
+        Column {
+            Row {
+                TextButton({}, Modifier.focusRequester(first)) { Text("First field") }
+                TextButton({}, Modifier.focusRequester(third)) { Text("Third field") }
+            }
+
+            Row {
+                TextButton({}, Modifier.focusRequester(second)) { Text("Second field") }
+                TextButton({}, Modifier.focusRequester(fourth)) { Text("Fourth field") }
+            }
+
+        }
+        TextButton(
+            onClick = {},
+            modifier = Modifier
+                .focusRequester(fourth)
+                .focusProperties {
+                    down = third
+                    right = second
+                }
+        ) {}
     }
 
 }
