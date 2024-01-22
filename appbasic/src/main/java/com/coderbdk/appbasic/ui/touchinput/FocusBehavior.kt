d39de0fa27c648da2399cbba383e9e803b7296fe
CoderBDK <cobdgithub07@gmail.com>
2024-01-22 05:38:15 +0000

    Input - Change Focus Behavior

---
+++ appbasic/src/main/java/com/coderbdk/appbasic/ui/touchinput/FocusBehavior.kt
@@ -0,0 +1,70 @@
+package com.coderbdk.appbasic.ui.touchinput
+
+import androidx.compose.foundation.focusGroup
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+/**
+ * Created by MD. ABDULLAH on Mon, Jan 22, 2024.
+ */
+
+@Preview
+@Composable
+fun FocusBehaviour() {
+    val chocolates = listOf(
+        0,
+        0,
+        1
+    )
+    Column(
+        Modifier.fillMaxSize()
+    ) {
+        LazyVerticalGrid(columns = GridCells.Fixed(4)) {
+            item(span = { GridItemSpan(maxLineSpan) }) {
+                Row(modifier = Modifier.focusGroup()) {
+                    FilterChipA()
+                    FilterChipB()
+                    FilterChipC()
+                }
+            }
+            items(chocolates) {
+                SweetsCard(sweets = it)
+            }
+        }
+    }
+}
+
+@Composable
+fun SweetsCard(sweets: Int) {
+    Card(
+        Modifier.fillMaxSize()
+    ) {
+        Text(text = "$sweets")
+    }
+}
+
+@Composable
+fun FilterChipC() {
+    Text(text = "Chip")
+}
+
+@Composable
+fun FilterChipB() {
+    Text(text = "Chip")
+}
+
+@Composable
+fun FilterChipA() {
+    Text(text = "Chip")
+}
+
