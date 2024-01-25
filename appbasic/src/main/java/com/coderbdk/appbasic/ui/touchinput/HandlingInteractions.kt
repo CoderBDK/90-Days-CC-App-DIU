40720e5b7fdf9272c3be9e1c7ce0428fcd4c7617
CoderBDK <cobdgithub07@gmail.com>
2024-01-25 10:53:37 +0000

    Handling Interactions

--- appbasic/src/main/java/com/coderbdk/appbasic/ui/touchinput/HandlingInteractions.kt
+++ appbasic/src/main/java/com/coderbdk/appbasic/ui/touchinput/HandlingInteractions.kt
@@ -1,13 +1,19 @@
 package com.coderbdk.appbasic.ui.touchinput
 
+import androidx.compose.foundation.hoverable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsPressedAsState
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 /**
  * Created by MD. ABDULLAH on Wed, Jan 24, 2024.
@@ -16,6 +22,7 @@ import androidx.compose.ui.tooling.preview.Preview
 @Composable
 @Preview
 fun HandlingInteraction() {
+    // This InteractionSource will emit hover interactions
     val interactionSource = remember { MutableInteractionSource() }
     val isPressed by interactionSource.collectIsPressedAsState()
 
@@ -23,5 +30,15 @@ fun HandlingInteraction() {
         onClick = { /* do something */ },
         interactionSource = interactionSource) {
         Text(if (isPressed) "Pressed!" else "Not pressed")
+    }
+
+
+    Box(
+        Modifier
+            .size(100.dp)
+            .hoverable(interactionSource = interactionSource),
+        contentAlignment = Alignment.Center
+    ) {
+        Text("Hello!")
     }
 }
