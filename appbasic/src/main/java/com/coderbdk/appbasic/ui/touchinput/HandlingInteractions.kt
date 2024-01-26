02ed09a5ed0f878a06da139750df08f4b9bace17
CoderBDK <cobdgithub07@gmail.com>
2024-01-26 09:07:34 +0000

    Handling Focusable Interactions

--- appbasic/src/main/java/com/coderbdk/appbasic/ui/touchinput/HandlingInteractions.kt
+++ appbasic/src/main/java/com/coderbdk/appbasic/ui/touchinput/HandlingInteractions.kt
@@ -1,10 +1,14 @@
 package com.coderbdk.appbasic.ui.touchinput
 
+import androidx.compose.foundation.IndicationInstance
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.hoverable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -12,6 +16,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -22,6 +27,7 @@ import androidx.compose.ui.unit.dp
 @Composable
 @Preview
 fun HandlingInteraction() {
+    /*
     // This InteractionSource will emit hover interactions
     val interactionSource = remember { MutableInteractionSource() }
     val isPressed by interactionSource.collectIsPressedAsState()
@@ -37,6 +43,35 @@ fun HandlingInteraction() {
         Modifier
             .size(100.dp)
             .hoverable(interactionSource = interactionSource),
+        contentAlignment = Alignment.Center
+    ) {
+        Text("Hello!")
+    } */
+
+    Column {
+        Focusable()
+    }
+
+}
+
+@Composable
+fun Focusable() {
+    // This InteractionSource will emit hover, focus, and press interactions
+    val interactionSource = remember { MutableInteractionSource() }
+
+    Box(
+        Modifier
+            .size(100.dp)
+            .clickable(
+                onClick = {},
+                interactionSource = interactionSource,
+
+                // Also show a ripple effect
+                indication = rememberRipple(
+                    color = Color.Green,
+                    radius = 40.dp
+                )
+            ),
         contentAlignment = Alignment.Center
     ) {
         Text("Hello!")
